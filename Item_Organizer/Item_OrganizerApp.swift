98b477036579a7abde77ce9d9be75f3736d0a236
colaboy82 <colaboy82@gmail.com>
2023-05-15 13:42:33 -0400

    Initial Commit

---
+++ Item_Organizer/Item_OrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Item_OrganizerApp.swift
+//  Item_Organizer
+//
+//  Created by Derrick Mu on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Item_OrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
