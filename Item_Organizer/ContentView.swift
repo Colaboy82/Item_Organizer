5eb7a62fb48da769788ec01850581f3cfad3c6be
colaboy82 <colaboy82@gmail.com>
2023-05-15 13:47:29 -0400

    Testing Derrick Branch for main menu testing

--- Item_Organizer/ContentView.swift
+++ Item_Organizer/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world! Commit Test")
+            Text("Hello, world! Commit Test, Testign Derrick Branch")
         }
         .padding()
     }
