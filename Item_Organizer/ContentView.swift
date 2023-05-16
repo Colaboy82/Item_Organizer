6119be6f9cc73541be8704b3d57598f76b00c2cd
Mu, Mitchell S <mmu34@gatech.edu>
2023-05-16 16:51:03 -0400

    initial nav bar design start

--- Item_Organizer/ContentView.swift
+++ Item_Organizer/ContentView.swift
@@ -2,23 +2,90 @@
 //  ContentView.swift
 //  Item_Organizer
 //
-//  Created by Derrick Mu on 5/15/23.
+//  Created by Mitchell Mu on 5/16/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var searchText = ""
+    @State private var isSearchExpanded = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world! Commit Test, This for Mitchell")
+        NavigationView {
+            VStack {
+                if isSearchExpanded {
+                    SearchBar(text: $searchText, isExpanded: $isSearchExpanded)
+                }
+                
+                Divider() // Horizontal line
+                
+                Text("Searching for \(searchText)")
+                    .padding()
+                
+                Spacer()
+            }
+            .navigationBarItems(
+                leading: leadingNavigationBarItem,
+                trailing: trailingNavigationBarItem
+            )
         }
-        .padding()
+    }
+    
+    var leadingNavigationBarItem: some View {
+        if !isSearchExpanded {
+            return AnyView(
+                Button(action: {
+                    // Home button action
+                }) {
+                    Image(systemName: "house.fill")
+                }
+            )
+        } else {
+            return AnyView(EmptyView())
+        }
+    }
+    
+    var trailingNavigationBarItem: some View {
+        if !isSearchExpanded {
+            return AnyView(
+                Button(action: {
+                    isSearchExpanded.toggle()
+                }) {
+                    Image(systemName: "magnifyingglass")
+                }
+            )
+        } else {
+            return AnyView(EmptyView())
+        }
     }
 }
 
+struct SearchBar: View {
+    @Binding var text: String
+    @Binding var isExpanded: Bool
+    
+    var body: some View {
+        HStack {
+            TextField("Search", text: $text)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button(action: {
+                isExpanded.toggle()
+            }) {
+                Text("Cancel")
+            }
+            .padding(.leading, 8)
+        }
+        .padding(.horizontal)
+    }
+}
+
+
+
+
+
+// MARK: --- This is the Preview Section ---
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
