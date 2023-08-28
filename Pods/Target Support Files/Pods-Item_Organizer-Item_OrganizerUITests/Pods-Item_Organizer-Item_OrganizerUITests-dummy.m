32bafd286bfc915d7711722073eedd9eee965d46
colaboy82 <colaboy82@gmail.com>
2023-08-27 21:01:01 -0700

    Added Firebase and CocoaPods

---
+++ Pods/Target Support Files/Pods-Item_Organizer-Item_OrganizerUITests/Pods-Item_Organizer-Item_OrganizerUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Item_Organizer_Item_OrganizerUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Item_Organizer_Item_OrganizerUITests
+@end
---
+++ Pods/Target Support Files/Pods-Item_OrganizerTests/Pods-Item_OrganizerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Item_OrganizerTests : NSObject
+@end
+@implementation PodsDummy_Pods_Item_OrganizerTests
+@end
